f94c929f80a58f5158d16aa88ff138d8a60c5202
YSAS4331 <ryo.matsu0619@gmail.com>
2025-12-17 23:47:50 +0900

    Create password.js

---
+++ password.js
@@ -0,0 +1,29 @@
+const DOCUMENT = document.body.innerHTML;
+
+const SALT = 'A9f!zQ7#Lm2@xYp$R4tVwZ8&';
+const HASH = '347e3f0e20e1e6836692eca83432885f9c04ef15d319bdd4476a41327f7aa1f1';
+
+async function makeHash(password, salt) {
+  const buf = new TextEncoder().encode(password + salt);
+  const hash = await crypto.subtle.digest("SHA-256", buf);
+  return Array.from(new Uint8Array(hash))
+              .map(b => b.toString(16).padStart(2, "0"))
+              .join("");
+}
+
+async function checkPw() {
+  const vl = document.getElementById("pw").value;
+  const has = await makeHash(vl, SALT);
+
+  if (has === HASH) {
+    document.body.innerHTML = DOCUMENT;
+  } else {
+    alert("パスワードが違います！");
+  }
+}
+
+document.body.innerHTML = `
+  <p>パスワードを入力してください</p>
+  <input type="password" id="pw" placeholder="password">
+  <button onclick="checkPw()">送信</button>
+`;
