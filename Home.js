59f90febab488a01e137c21a714d68c0d1cec73c
YSAS4331 <ryo.matsu0619@gmail.com>
2025-12-20 22:02:00 +0900

    Update Home.js

--- Home.js
+++ Home.js
@@ -2,6 +2,10 @@
   const { $,$$ } = window.modules;
   const res = await fetch('./videos.json');
   const datas = await res.json();
+  const getQueryParam = (urlStr, key) => {
+    const url = new URL(urlStr);
+    return url.searchParams.get(key);
+  };
 
   const popular = [...datas].sort((a, b) => {
     const scoreA = a.likeCount * 100 + a.viewCount;
@@ -12,7 +16,7 @@
   const likeCountHtml = popular.map(video => {
     return `
       <div class="listItem">
-        <a href="${video.url}">${video.title}</a>
+        <a href="${video.url}" title="${video.title}"><img src="https://img.youtube.com/vi/${getQueryParam(video.url, 'v')}/maxresdefault.jpg" alt="サムネイル画像"></a>
         <div class="like">${video.likeCount}</div>
         <div class="view">${video.viewCount}</div>
       </div>
